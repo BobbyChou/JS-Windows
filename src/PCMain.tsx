9868e99b8450defa40241daf30daedf529843f06
周腾 <zhouteng@yodinfo.com>
2020-11-24 13:36:52 +0800

    nothing to commit

--- src/PCMain.tsx
+++ src/PCMain.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './App.less'
 import { HashRouter, Switch, Route, Redirect } from 'react-router-dom'
+
 import Login from './Views/Login/Login'
 import Home from './Views/Home/Home'
+import Mine from './Views/Mine/Mine'
 
 const PCMain: React.FC = () => {
   const handleRenderRedirect = (path: string) => {
@@ -16,6 +18,7 @@ const PCMain: React.FC = () => {
           <Route exact path="/" component={() => { return handleRenderRedirect('home') }}></Route>
           <Route path={'/login'} component={Login}></Route>
           <Route path={'/home'} component={() => { return localStorage.getItem('Account') ? <Home /> : handleRenderRedirect('home') }}></Route>
+          <Route path={'/mine'} component={() => { return localStorage.getItem('Account') ? <Mine /> : handleRenderRedirect('mine') }}></Route>
         </Switch>
       </HashRouter>
     )
--- src/Views/Mine/Mine.tsx
+++ src/Views/Mine/Mine.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Mine.less'
 
-const Mine: React.FC = () => {
-    return (
-        <div>mine init</div>
-    )
+const Mine: React.FC = (props) => {
+
+  const testFn = (name: string): void => {
+    console.log(111)
+  }
+
+  return (
+    <div>123</div>
+  )
 }
 
 export default Mine
